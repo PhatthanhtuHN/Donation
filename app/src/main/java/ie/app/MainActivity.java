9f0965f782c86ba9ac01e2b3b47d2e0f05988802
PhatthanhtuHN <SRV.Vietnamese@outlook.com>
2021-12-08 12:05:44 +0700

    Week5

--- app/src/main/java/ie/app/MainActivity.java
+++ app/src/main/java/ie/app/MainActivity.java
@@ -22,6 +22,7 @@ import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.NumberPicker;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -80,13 +81,14 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId())
+        {
+            case R.id.menuReport:
+                Toast toast = Toast.makeText(this, "Report Selected",
+                        Toast.LENGTH_SHORT);
+                toast.show();
+                break;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
