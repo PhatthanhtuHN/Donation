55dc31afa9f4df0dc4adca91bac79ac97676423a
PhatthanhtuHN <SRV.Vietnamese@outlook.com>
2021-12-21 00:00:09 +0700

    Week10

--- app/src/main/java/ie/app/activities/Base.java
+++ app/src/main/java/ie/app/activities/Base.java
@@ -1,6 +1,7 @@
 package ie.app.activities;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -11,27 +12,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ie.activities.R;
+import ie.app.main.DonationApp;
 import ie.app.models.Donation;
 
 public class Base extends AppCompatActivity {
-    public final int target = 10000;
-    public int totalDonated = 0;
-    public static List<Donation> donations = new ArrayList<Donation>();
-    public boolean newDonation(Donation donation)
-    {
-        boolean targetAchieved = totalDonated > target;
-        if (!targetAchieved)
-        {
-            donations.add(donation);
-            totalDonated += donation.amount;
-        }
-        else
-        {
-            Toast toast = Toast.makeText(this, "Target Exceeded!", Toast.LENGTH_SHORT);
-            toast.show();
-        }
-        return targetAchieved;
+    public DonationApp app;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        app = (DonationApp) getApplication();
+        app.dbManager.open();
+        app.dbManager.setTotalDonated(this);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
@@ -43,18 +37,28 @@ public class Base extends AppCompatActivity {
         super.onPrepareOptionsMenu(menu);
         MenuItem report = menu.findItem(R.id.menuReport);
         MenuItem donate = menu.findItem(R.id.menuDonate);
-        if(donations.isEmpty())
+        MenuItem reset = menu.findItem(R.id.menuReset);
+        if(app.dbManager.getAll().isEmpty())
+        {
             report.setEnabled(false);
-        else
+            reset.setEnabled(false);
+        }
+        else {
             report.setEnabled(true);
+            reset.setEnabled(true);
+        }
         if(this instanceof MainActivity){
             donate.setVisible(false);
-            if(!donations.isEmpty())
+            if(!app.dbManager.getAll().isEmpty())
+            {
                 report.setVisible(true);
+                reset.setEnabled(true);
+            }
         }
         else {
             report.setVisible(false);
             donate.setVisible(true);
+            reset.setVisible(false);
         }
         return true;
     }
